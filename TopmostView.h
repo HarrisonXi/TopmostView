b5920e187e4b7b7a8feeb54689cfc8205b824001
HarrisonXi <gpra8764@gmail.com>
2016-02-19 15:42:38 +0800

    iOS 9 implement

--- TopmostView.h
+++ TopmostView.h
@@ -11,9 +11,9 @@
 @interface TopmostView : UIView
 
 // get or create a new topmost view for the application window
-+ (TopmostView *)viewForApplicationWindow;
++ (instancetype)viewForApplicationWindow;
 
 // get or create a new topmost view for the specified window
-+ (TopmostView *)viewForWindow:(UIWindow *)window;
++ (instancetype)viewForWindow:(UIWindow *)window;
 
 @end
--- TopmostView.m
+++ TopmostView.m
@@ -8,16 +8,58 @@
 
 #import "TopmostView.h"
 
+@interface TopmostView ()
+
+@property (nonatomic, weak) UIWindow *window;
+
+@end
+
 @implementation TopmostView
 
-+ (TopmostView *)viewForApplicationWindow
+- (instancetype)initWithWindow:(UIWindow *)window
 {
-    return [TopmostView new];
+    if (self = [super init]) {
+        self.backgroundColor = [UIColor clearColor];
+        self.userInteractionEnabled = NO;
+        self.window = window;
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeOrientationAction:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+    }
+    return self;
 }
 
-+ (TopmostView *)viewForWindow:(UIWindow *)window
+- (void)dealloc
 {
-    return [TopmostView new];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+}
+
+- (void)changeOrientationAction:(NSNotification *)notification
+{
+    [UIView animateWithDuration:UINavigationControllerHideShowBarDuration animations:^{
+        self.frame = self.window.bounds;
+    }];
+}
+
++ (instancetype)viewForApplicationWindow
+{
+    return [self viewForWindow:[UIApplication sharedApplication].keyWindow];
+}
+
++ (instancetype)viewForWindow:(UIWindow *)window
+{
+    TopmostView *topmostView = nil;
+    for (UIView *subview in window.subviews) {
+        if ([subview isKindOfClass:[TopmostView class]]) {
+            topmostView = (TopmostView *)subview;
+            break;
+        }
+    }
+    if (!topmostView) {
+        topmostView = [[self alloc] initWithWindow:window];
+        topmostView.frame = window.bounds;
+        [window addSubview:topmostView];
+    }
+    [topmostView.window bringSubviewToFront:topmostView];
+    return topmostView;
 }
 
 @end
